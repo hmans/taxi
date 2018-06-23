6a431a4f16198366de29f4c25dac85775fca6638
Hendrik Mans <hendrik@mans.de>
2018-06-23 13:20:29 +0200

    Make it work.

--- spec/hypertux_spec.cr
+++ spec/hypertux_spec.cr
@@ -1,9 +1,22 @@
 require "./spec_helper"
 
 describe Hypertux do
-  # TODO: Write tests
+  it "renders a tag with some text" do
+    str = Hypertux.run do
+      h1 "Hello world!"
+    end
 
-  it "works" do
-    false.should eq(true)
+    str.should eq("<h1>Hello world!</h1>")
+  end
+
+  it "renders a tree of tags" do
+    str = Hypertux.run do
+      div [
+        p("Hello world!"),
+        p("Here's another paragraph.")
+      ]
+    end
+
+    str.should eq("<div><p>Hello world!</p><p>Here's another paragraph.</p></div>")
   end
 end
--- src/hypertux.cr
+++ src/hypertux.cr
@@ -1,6 +1,36 @@
 require "./hypertux/*"
 
-# TODO: Write documentation for `Hypertux`
 module Hypertux
-  # TODO: Put your code here
+  def self.tag(name, children : Array(String))
+    String::Builder.new.tap do |b|
+      b << "<#{name}>"
+
+      children.each do |child|
+        b << child
+      end
+
+      b << "</#{name}>"
+    end.to_s
+  end
+
+  def self.tag(name, children : String)
+    tag(name, [children])
+  end
+
+  def self.run
+    with self yield
+  end
+
+  # Shortcut macros
+  macro h1(*args)
+    tag(:h1, {{ *args }})
+  end
+
+  macro div(*args)
+    tag(:div, {{ *args }})
+  end
+
+  macro p(*args)
+    tag(:p, {{ *args }})
+  end
 end
